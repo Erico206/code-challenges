686b69f17e2c5737e58cc2593e20e59c5c70b24b
Erico206 <erico@avvo.com>
2018-12-10 21:47:15 -0800

    unique array

---
+++ lab1.js
@@ -0,0 +1,15 @@
+const uniqueArray = ( array ) => {
+  let counts = {};
+  let newArray = [];
+  let j = 0;
+
+  for( let i = 0; i < array.length ; i++ ) {
+
+    let item = array[ i ];
+    if( counts[ item ] !== 1 ) {
+      counts[ item ] = 1;
+      newArray[ j++ ] = item;
+    }
+  }
+  return newArray;
+};
