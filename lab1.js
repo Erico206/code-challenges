026391c1d357514b7b590d0fca34028463872b8d
Erico206 <erico@avvo.com>
2018-12-10 22:02:11 -0800

    reversal

--- lab1.js
+++ lab1.js
@@ -1,9 +1,30 @@
+
+// delete duplicates
+
 const uniqueArray = ( array ) => {
   let counts   = {};
   let newArray = [];
   let counter  = 0;
 
   for( let i = 0; i < array.length ; i++ ) {
+    let item = array[ i ];
+    if( counts[ item ] !== 1 ) {
+      counts[ item ]  = 1;
+      newArray[ counter++ ] = item;
+    }
+  }
+  return newArray;
+};
+
+//reverse an array
+
+
+const uniqueArray = ( array ) => {
+  let counts   = {};
+  let newArray = [];
+  let counter  = 0;
+
+  for( let i = array.length - 1; i >= 0; i-- ) {
     let item = array[ i ];
     if( counts[ item ] !== 1 ) {
       counts[ item ]  = 1;
