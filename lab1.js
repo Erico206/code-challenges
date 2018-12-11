fe8c33d0f82679ad5f4d333b47d5486b0dd28264
Erico206 <erico@avvo.com>
2018-12-10 22:14:14 -0800

    tweaks to duplicate and reversal added

--- lab1.js
+++ lab1.js
@@ -9,17 +9,16 @@ const uniqueArray = ( array ) => {
   for( let i = 0; i < array.length ; i++ ) {
     let item = array[ i ];
     if( counts[ item ] !== 1 ) {
-      counts[ item ]  = 1;
+      counts[ item ] = 1;
       newArray[ counter++ ] = item;
     }
   }
   return newArray;
 };
 
-//reverse an array
+//reverse a Unique Array
 
-
-const uniqueArray = ( array ) => {
+const reverseUniqueArray = ( array ) => {
   let counts   = {};
   let newArray = [];
   let counter  = 0;
@@ -27,9 +26,21 @@ const uniqueArray = ( array ) => {
   for( let i = array.length - 1; i >= 0; i-- ) {
     let item = array[ i ];
     if( counts[ item ] !== 1 ) {
-      counts[ item ]  = 1;
+      counts[ item ] = 1;
       newArray[ counter++ ] = item;
-    }
-  }
+    };
+  };
+  return newArray;
+};
+
+//reverse array
+const reverseArray = ( array ) => {
+
+  let newArray = [];
+  let counter  = 0;
+
+  for ( let i = array.length - 1; i >= 0; i-- ) {
+    newArray[ counter++ ] = array[ i ];
+  };
   return newArray;
 };
